7bd93999d330ab0745dfc1562f96183f92672aab
szymo <szymonsm25@gmail.com>
2023-10-24 19:41:20 +0200

    create different project structure and initiate db

--- CODE/stockdropapp/app/webapp.py
+++ CODE/stockdropapp/app/webapp.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/szymo/LBN-INZ/CODE/stockdropapp/your_database.db'  # Change to your database URL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # Change to your database URL
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
