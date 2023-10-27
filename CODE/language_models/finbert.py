9336440077792175a4d8ceacce75ebcd3bf96b2a
wojtek <wojciech.kosiuk@gmail.com>
2023-10-27 12:59:08 +0200

    corrected adding finbert columns

--- CODE/language_models/finbert.py
+++ CODE/language_models/finbert.py
@@ -15,7 +15,7 @@ class FinBERT:
     def __init__(self, device: int = -1) -> None:
         self.pipe = pipeline("text-classification", model="ProsusAI/finbert", device=device)
 
-    def pipeline_predict_sentiment(self, texts: str | list[str]) -> list:
+    def pipeline_predict_sentiment(self, texts: str | list[str]) -> pd.DataFrame:
         """
         Predicts sentiment of a text or list of texts.
         # TODO: add option to return probabilities
@@ -26,10 +26,14 @@ class FinBERT:
         # return tqdm(self.pipe(texts))
         predictions = []
         for text in tqdm.tqdm(texts):
-            predictions.append(self.pipe(text))
+            tmp_prediction = self.pipe(text)
+            predictions += tmp_prediction
+
+        predictions = pd.json_normalize(predictions)
+        predictions.rename(columns={"label": "finbert_label", "score": "finbert_score"}, inplace=True)
         return predictions
     
-    def add_predictions_to_df(df: pd.DataFrame, predictions: list) -> pd.DataFrame:
+    def add_predictions_to_df(df: pd.DataFrame, predictions: pd.DataFrame) -> pd.DataFrame:
         """
         Adds predictions to a dataframe.
 
@@ -37,8 +41,7 @@ class FinBERT:
         :param predictions: list, predictions to add to dataframe
         :return: pd.DataFrame, dataframe with predictions added
         """
-        return pd.concat([df,pd.DataFrame(predictions)], axis=1)
-    
+        return pd.concat([df, predictions], axis=1)
     
 
 def main() -> None:
@@ -46,14 +49,15 @@ def main() -> None:
     # If you have GPU, set device=0, else set device=-1
     device = -1
 
-    # finbert = FinBERT(device)
+    finbert = FinBERT(device)
 
-    # df = pd.read_csv("DATA/alphavantage/news/CRYPTOBTC/CRYPTOBTC_20230101_20231001_example.csv")
-    # predictions = finbert.pipeline_predict_sentiment(list(df["summary"]))
-    # df_new = FinBERT.add_predictions_to_df(df, predictions)
+    df = pd.read_csv("DATA/alphavantage/news/BA/BA_20230315_20230430.csv")
+    predictions = finbert.pipeline_predict_sentiment(list(df["summary"]))
+
+    df_new = FinBERT.add_predictions_to_df(df, predictions)
     # df_new.to_csv("test.csv")
 
-    FinBERT.model.save_pretrained("MODELS/finbert/")
+    # FinBERT.model.save_pretrained("MODELS/finbert/")
     
 if __name__ == "__main__":
     main()
