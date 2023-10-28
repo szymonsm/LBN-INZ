0b239348fc8d357b52e78663115d18339fb665a2
szymo <szymonsm25@gmail.com>
2023-10-28 22:58:53 +0200

    close to create a basic plot

--- CODE/stockdropapp2/app.py
+++ CODE/stockdropapp2/app.py
@@ -1,11 +1,13 @@
 import yaml
-from flask import Flask, render_template, render_template, request, redirect, url_for
+from flask import Flask, render_template, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect
 from flask_migrate import Migrate
 import pandas as pd
 from datetime import datetime, timedelta
 import requests
+import plotly.graph_objs as go
+import plotly.express as px
 import re
 import os
 
@@ -305,6 +307,34 @@ def display_tables():
     news = News.query.all()
     columns_news = [column.key for column in inspect(News).c]
     return render_template('display_tables.html', stock_prices=stock_prices, news=news, columns_prices=columns_prices, columns_news=columns_news)
+
+@app.route('/update_plot', methods=['POST'])
+def update_plot():
+    selected_currency = request.json.get('currency')
+    selected_time_period = request.json.get('time_period')
+    selected_data_type = request.json.get('data_type')
+
+    # Modify the query to filter data based on selected criteria
+    data = StockPrice.query.filter_by(
+        ticker=selected_currency,
+        interval=selected_time_period
+    ).with_entities(StockPrice.timestamp, getattr(StockPrice, selected_data_type)).all()
+
+    timestamps = [pd.to_datetime(row[0]) for row in data]
+    price_data = [row[1] for row in data]
+    df = pd.DataFrame(dict(
+        x = timestamps,
+        y = price_data
+    ))
+    print(df.head())
+
+    # Create the Plotly figure
+    # fig = go.Figure(data=go.Scatter(x=timestamps, y=price_data, mode='lines', name=selected_data_type))
+    fig = px.line(df, x='x', y='y', title=f'{selected_currency} {selected_data_type} {selected_time_period}')
+
+    # Convert the Plotly figure to JSON format for AJAX response
+    plot_data = fig.to_json()
+    return jsonify(plot_data)
 
 # @app.route('/start_daemon/<currency>/<time_period>/<model>')
 # def start_daemon_route(currency, time_period, model):
---
+++ CODE/stockdropapp2/static/js/script.js
@@ -0,0 +1,95 @@
+const runningDaemons = {};
+
+let isDaemonRunning = false;
+
+function startDaemon() {
+    const currency = document.getElementById('currency').value;
+    const time_period = document.getElementById('time_period').value;
+
+    // Check if daemon is running for the current currency and time period
+    if (runningDaemons[`${currency}_${time_period}`]) {
+        // Daemon is running, so it can be disabled
+        stopDaemon(`${currency}_${time_period}`);
+    } else {
+        // Daemon is not running, start it
+        fetch(`/start_daemon/${currency}/${time_period}/Model1`)
+            .then(response => response.text())
+            .then(data => {
+                alert(data);
+                // Add the running daemon to the list
+                runningDaemons[`${currency}_${time_period}`] = `Daemon for ${currency}, ${time_period} running...`;
+                updateRunningDaemons();
+            });
+    }
+}
+
+function updateRunningDaemons() {
+    const runningDaemonsList = document.getElementById('running_daemons');
+    runningDaemonsList.innerHTML = '';  // Clear previous entries
+
+    for (const key in runningDaemons) {
+        const li = document.createElement('li');
+        li.innerText = runningDaemons[key];
+        // const stopButton = document.createElement('button');
+        // stopButton.innerText = 'Stop';
+        // stopButton.onclick = () => stopDaemon(key);
+        // li.appendChild(stopButton);
+        runningDaemonsList.appendChild(li);
+    }
+
+    // Check if a daemon is running for the current currency and time period
+    const currency = document.getElementById('currency').value;
+    const time_period = document.getElementById('time_period').value;
+    isDaemonRunning = !!runningDaemons[`${currency}_${time_period}`];
+    const button = document.getElementById('notification_toggle');
+    button.innerText = isDaemonRunning ? 'Disable Notification' : 'Enable Notification';
+}
+
+
+function stopDaemon(key) {
+    // Implement logic to stop the daemon based on the key
+    runningDaemons[key] = `Daemon for ${key} stopped.`;
+    updateRunningDaemons();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to update the plot
+    function updatePlot() {
+        // Get the selected currency, time period, and data type
+        const selectedCurrency = document.getElementById('currency').value;
+        const selectedTimePeriod = document.getElementById('time_period').value;
+        const selectedData = document.getElementById('data_type').value;
+
+        // Make an AJAX request to the server to fetch updated plot data
+        fetch('/update_plot', {
+            method: 'POST',
+            body: JSON.stringify({
+                currency: selectedCurrency,
+                time_period: selectedTimePeriod,
+                data_type: selectedData,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Update the Plotly plot with the new data
+                const plotDiv = document.getElementById('plot-div');
+                Plotly.newPlot(plotDiv, data);
+            });
+    }
+
+    // Get the data type select box element
+    const dataTypeSelect = document.getElementById('data_type');
+    const currencySelect = document.getElementById('currency');
+    const time_periodSelect = document.getElementById('time_period');
+
+    // Add an event listener to capture the change event
+    dataTypeSelect.addEventListener('change', updatePlot);
+    currencySelect.addEventListener('change', updatePlot);
+    time_periodSelect.addEventListener('change', updatePlot);
+
+    // Initialize the plot when the page loads
+    updatePlot();
+});
