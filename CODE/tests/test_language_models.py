90b3d9867dc0758e79219b70fc4ba8f7be471554
wojtek <wojciech.kosiuk@gmail.com>
2023-11-24 21:04:13 +0100

    some unfinished language models tests

---
+++ CODE/tests/test_language_models.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from CODE.language_models.finbert import FinBERT
+from CODE.language_models.bart_large_mnli import BartLargeMNLI
+from CODE.language_models.vader import Vader
+
+
+class TestLanguageModels(TestCase):
+
+    def test_finbert(self):
+        finbert = FinBERT()
+        print(finbert.pipeline_predict_sentiment("Apple is gonna skyrocket by 20%"))
+
+    def test_bart_large_mnli(self):
+        bart = BartLargeMNLI()
+        bart.initialize_model()
+        print(bart.predict_classes("Apple is gonna skyrocket by 20%", ["Bullish", "Bearish"]))
+
+    def test_vader(self):
+        vader = Vader()
+        print(vader.sentiment_scores("Apple is gonna skyrocket by 20%"))
+        print(vader.pipeline_predict_sentiment("Apple is gonna skyrocket by 20%"))
+    
