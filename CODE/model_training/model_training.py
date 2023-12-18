a6c88f8dfcbb6517d53c513034049c59a4be7127
szymo <szymonsm25@gmail.com>
2023-12-18 22:00:13 +0100

    NBEATS, NHITS, TFT working

--- CODE/model_training/model_training.py
+++ CODE/model_training/model_training.py
@@ -1,24 +1,23 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 import optuna
-from neuralforecast.losses.pytorch import DistributionLoss, MQLoss, RMSE
-from neuralforecast.models import FEDformer, NBEATS, NHITS
+from neuralforecast.models import NBEATS, NHITS, TFT
 from neuralforecast import NeuralForecast
 from functools import partial
 
-FINAL_COLS = ['mean_future', 'mean_influential','mean_trustworthy', 'mean_clickbait','norm_rsi_14', 'norm_rsi_gspc_14', 'norm_slowk_14']
-NAME_MODEL = {"NBEATS": NBEATS, "NHITS": NHITS, "FEDformer": FEDformer}
+#FINAL_COLS = ['mean_future', 'mean_influential','mean_trustworthy', 'mean_clickbait','norm_rsi_14', 'norm_rsi_gspc_14', 'norm_slowk_14']
+NAME_MODEL = {"NBEATS": NBEATS, "NHITS": NHITS, "TFT": TFT}
 
-def create_trial_params_fedformer(trial):
+def create_trial_params_tft(trial):
+
     input_size = trial.suggest_int('input_size', 2, 10)
-    
-    scaler_type = trial.suggest_categorical('scaler_type', ['standard'])
     learning_rate = trial.suggest_loguniform('learning_rate', 1e-5, 1e-1)
+    hidden_size = trial.suggest_categorical('hidden_size', [4, 8, 12, 16, 20, 24, 28, 32])
+    dropout = trial.suggest_float('dropout', 0.0, 0.5)
+    attn_dropout = trial.suggest_float('attn_dropout', 0.0, 0.5)
 
-    model_params = {"input_size": input_size, "max_steps": 300, "hist_exog_list": FINAL_COLS,
-      "learning_rate": learning_rate, "scaler_type": scaler_type, "random_seed": 1}
+    model_params = {"input_size": input_size, "learning_rate": learning_rate, "hidden_size": hidden_size, "dropout": dropout, "attn_dropout": attn_dropout}
 
     return model_params
 
@@ -37,44 +36,48 @@ def create_trial_params_nbeats(trial):
     n_harmonics = trial.suggest_int('n_harmonics', 1, 5)
     n_polynomials = trial.suggest_int('n_polynomials', 1, 5)
     
-    scaler_type = trial.suggest_categorical('scaler_type', ['standard'])
     learning_rate = trial.suggest_loguniform('learning_rate', 1e-5, 1e-1)
     
     
     n_blocks = [n_blocks_season, n_blocks_trend, n_blocks_identity]
     mlp_units=[[mlp_units_n, mlp_units_n]]*num_hidden
     
-    model_params = {"input_size": input_size, "max_steps": 300, "hist_exog_list": FINAL_COLS,
+    model_params = {"input_size": input_size,
       "stack_types": ['seasonality', 'trend', 'identity'], "mlp_units": mlp_units, "n_blocks": n_blocks,
-      "n_harmonics": n_harmonics, "n_polynomials": n_polynomials, "learning_rate": learning_rate,
-      "scaler_type": scaler_type, 'random_seed': 1 }
+      "n_harmonics": n_harmonics, "n_polynomials": n_polynomials, "learning_rate": learning_rate}
     
     return model_params
 
 def create_trial_params_nhits(trial):
     input_size = trial.suggest_int('input_size', 2, 20)
     
-    n_blocks_season = trial.suggest_int('n_blocks_season', 1, 3)
-    n_blocks_trend = trial.suggest_int('n_blocks_trend', 1, 3)
-    n_blocks_identity = trial.suggest_int('n_blocks_ident', 1, 3)
+    n_blocks1 = trial.suggest_int('n_blocks1', 1, 3)
+    n_blocks2 = trial.suggest_int('n_blocks2', 1, 3)
+    n_blocks3 = trial.suggest_int('n_blocks3', 1, 3)
     
     mlp_units_n = trial.suggest_categorical('mlp_units', [32, 64, 128])
-    num_hidden = trial.suggest_int('num_hidden', 1, 3)
     
-    n_harmonics = trial.suggest_int('n_harmonics', 1, 5)
-    n_polynomials = trial.suggest_int('n_polynomials', 1, 5)
+    n_pool_kernel_size1 = trial.suggest_int('n_pool_kernel_size1', 1, 3)
+    n_pool_kernel_size2 = trial.suggest_int('n_pool_kernel_size2', 1, 3)
+    n_pool_kernel_size3 = trial.suggest_int('n_pool_kernel_size3', 1, 3)
+
+    n_freq_downsample1 = trial.suggest_int('n_freq_downsample1', 1, 5)
+    n_freq_downsample2 = trial.suggest_int('n_freq_downsample2', 1, 5)
+    n_freq_downsample3 = trial.suggest_int('n_freq_downsample3', 1, 5)
     
-    scaler_type = trial.suggest_categorical('scaler_type', ['standard'])
     learning_rate = trial.suggest_loguniform('learning_rate', 1e-5, 1e-1)
+    dropout_prob_theta = trial.suggest_float('dropout_prob_theta', 0.0, 0.5)
     
     
-    n_blocks = [n_blocks_season, n_blocks_trend, n_blocks_identity]
-    mlp_units=[[mlp_units_n, mlp_units_n]]*num_hidden
+    n_blocks = [n_blocks1, n_blocks2, n_blocks3]
+    mlp_units=[[mlp_units_n, mlp_units_n]]*3
+    n_pool_kernel_size = [n_pool_kernel_size1, n_pool_kernel_size2, n_pool_kernel_size3]
+    n_freq_downsample = [n_freq_downsample1, n_freq_downsample2, n_freq_downsample3]
+    stack_types = ['identity', 'identity', 'identity']
     
-    model_params = {"input_size": input_size, "max_steps": 300, "hist_exog_list": FINAL_COLS,
-      "stack_types": ['seasonality', 'trend', 'identity'], "mlp_units": mlp_units, "n_blocks": n_blocks,
-      "n_harmonics": n_harmonics, "n_polynomials": n_polynomials, "learning_rate": learning_rate,
-      "scaler_type": scaler_type, 'random_seed': 1}
+    model_params = {"input_size": input_size, "mlp_units": mlp_units, "n_blocks": n_blocks, "stack_types": stack_types,
+    "learning_rate": learning_rate, "dropout_prob_theta": dropout_prob_theta, "n_pool_kernel_size": n_pool_kernel_size,
+    "n_freq_downsample": n_freq_downsample}
     
     return model_params
 
@@ -90,7 +93,7 @@ def pipeline_train_predict(models, train_set, val_set, horizon, loss_func, model
     val_set_tmp = val_set.iloc[:last_index]
     modelID = 0
     while train_set_tmp.iloc[0]['ds'] < val_set_begin and len(val_set_tmp) == horizon:
-        print(len(train_set_tmp), len(val_set_tmp))
+        #print(len(train_set_tmp), len(val_set_tmp))
         loss, predictions_tmp = train(models, train_set_tmp, val_set_tmp, loss_func, model_name)
         predictions_tmp['modelID'] = modelID
         modelID += 1
@@ -103,6 +106,10 @@ def pipeline_train_predict(models, train_set, val_set, horizon, loss_func, model
         # make val_set_tmp start from the next day after the last day of train_set_tmp
         val_set_tmp = val_set.iloc[last_index:last_index+horizon]
         last_index = last_index + horizon
+    # Add sequence ID and reset index
+    predictions['sequenceID'] = predictions.index
+    predictions.reset_index(drop=True, inplace=True)
+
     return np.mean(losses), predictions
 
 
@@ -127,20 +134,19 @@ def train(models: list, train_set: pd.DataFrame, val_set: pd.DataFrame, metric_f
     return metric_function(p['y'], p[model_name]), p
 
 def create_trial_params(trial, model_name: str):
-    if model_name == "FEDformer":
-      return create_trial_params_fedformer(trial)
+    if model_name == "TFT":
+      return create_trial_params_tft(trial)
     if model_name == "NBEATS":
       return create_trial_params_nbeats(trial)
     if model_name == "NHITS":
       return create_trial_params_nhits(trial)     
 
 
-def objective(trial, train_set, val_set, model_name: str, horizon: int, loss_func: callable):
+def objective(trial, train_set, val_set, loss, model_name: str, horizon: int, hist_exog_list: list, max_steps: int, random_seed: int, loss_func: callable, scaler_type = 'standard'):
     model_params = create_trial_params(trial, model_name)
-    print(model_params)
     # n_blocks = [prms['n_blocks_season'], prms['n_blocks_trend'], prms['n_blocks_ident']]
     # mlp_units=[[prms['mlp_units'], prms['mlp_units']]*prms['num_hidden']]
-    models = [NAME_MODEL[model_name](h=horizon, loss=RMSE(),**model_params)]
+    models = [NAME_MODEL[model_name](h=horizon, loss=loss, hist_exog_list=hist_exog_list, random_seed=random_seed, scaler_type=scaler_type, max_steps=max_steps, **model_params)]
 
     losses, predictions = pipeline_train_predict(models, train_set, val_set, horizon, loss_func, model_name)
     return np.mean(losses)
